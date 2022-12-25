60afda46cb03e466b65a5ccb3803790eeb457cb2
Sebastian Rodríguez <serogra15121999@gmail.com>
2022-12-25 18:13:03 -0500

    Commit inicial

---
+++ src/main/java/co/com/accenture/acmspimresttypicode/AcMsPimRestTypicodeApplication.java
@@ -0,0 +1,13 @@
+package co.com.accenture.acmspimresttypicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcMsPimRestTypicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcMsPimRestTypicodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/accenture/acmspimresttypicode/AcMsPimRestTypicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.accenture.acmspimresttypicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcMsPimRestTypicodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
